548053f25c6c51123db275f0ffc270a9376f3990
Sahel Mesforoush <5176161-sahel.m4@users.noreply.gitlab.com>
2021-04-01 09:15:28 +0430

    save best twenty matches

--- HW1/q3/q3-1.py
+++ HW1/q3/q3-1.py
@@ -1,3 +1,5 @@
+import random
+
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
@@ -24,11 +26,11 @@ def draw_match(base_image, position1, position2, color, r=5, thickness=5):
     cv2.line(base_image, position1, position2, color, thickness)
 
 
-def draw_inlier_outlier(draw_img, based_shape, matches, masks, kp1, kp2,
+def draw_inlier_outlier(draw_img, based_shape, matches, masks, img1_points, img2_points,
                         inlier_color=(255, 0, 0), outlier_color=(0, 255, 255)):
     for i, m in enumerate(matches):
-        end1 = (round(kp1[m.queryIdx].pt[0]), round(kp1[m.queryIdx].pt[1]))
-        end2 = (round(kp2[m.trainIdx].pt[0] + based_shape[1]), round(kp2[m.trainIdx].pt[1]))
+        end1 = (round(img1_points[i][0, 0]), round(img1_points[i][0, 1]))
+        end2 = (round(img2_points[i][0, 0] + based_shape[1]), round(img2_points[i][0, 1]))
         if masks[i]:
             draw_match(base_image=draw_img, position1=end1, position2=end2, color=inlier_color)
         else:
@@ -36,13 +38,13 @@ def draw_inlier_outlier(draw_img, based_shape, matches, masks, kp1, kp2,
     return draw_img
 
 
-def draw_matches(new_img, based_shape, kp1, kp2, matches, color=None):
+def draw_correspondents(new_img, based_shape, img1_points, img2_points, matches, color=None):
     r = 5
     thickness = 5
     img_with_line = new_img.copy()
     for i, m in enumerate(matches):
-        end1 = (round(kp1[m.queryIdx].pt[0]), round(kp1[m.queryIdx].pt[1]))
-        end2 = (round(kp2[m.trainIdx].pt[0] + based_shape[1]), round(kp2[m.trainIdx].pt[1]))
+        end1 = (round(img1_points[i][0, 0]), round(img1_points[i][0, 1]))
+        end2 = (round(img2_points[i][0, 0] + based_shape[1]), round(img2_points[i][0, 1]))
         cv2.circle(new_img, end1, r, color, thickness)
         cv2.circle(new_img, end2, r, color, thickness)
         draw_match(img_with_line, end1, end2, color)
@@ -73,16 +75,10 @@ if __name__ == '__main__':
     for i, (m, n) in enumerate(matches):
         if m.distance < 0.7 * n.distance:
             new_matches.append(m)
-    best_twenty_matches = sorted(new_matches, key=lambda x: x.distance)
+    # best_twenty_matches = sorted(new_matches, key=lambda x: x.distance)[:40]
+    best_twenty_matches = random.sample(new_matches, 20)
     best_twenty_images = cv2.drawMatches(img1, kp1, img2, kp2, best_twenty_matches, img2, flags=2, matchColor=(255, 0, 0))
     cv2.imwrite(output_path.format("res16-1.jpg"), best_twenty_images)
-
-    # draw_params = dict(matchColor=(0, 0, 0),
-    #                    singlePointColor=(0, 255, 0),
-    #                    flags=2)
-    img4, with_line = draw_matches(res, img1.shape, kp1, kp2, new_matches, color=(0, 0, 255))
-    cv2.imwrite(output_path.format("res14_correspondences.jpg"), img4)
-    cv2.imwrite(output_path.format("res15_match.jpg"), with_line)
 
     image_1_points = np.zeros((len(new_matches), 1, 2), dtype=np.float32)
     image_2_points = np.zeros((len(new_matches), 1, 2), dtype=np.float32)
@@ -91,11 +87,19 @@ if __name__ == '__main__':
         image_1_points[i] = kp1[new_matches[i].queryIdx].pt
         image_2_points[i] = kp2[new_matches[i].trainIdx].pt
 
+    # draw_params = dict(matchColor=(0, 0, 0),
+    #                    singlePointColor=(0, 255, 0),
+    #                    flags=2)
+    img4, with_line = draw_correspondents(res, img1.shape, image_1_points, image_2_points, new_matches, color=(0, 0, 255))
+    cv2.imwrite(output_path.format("res14_correspondences.jpg"), img4)
+    cv2.imwrite(output_path.format("res15_match.jpg"), with_line)
+
     homography, mask = cv2.findHomography(image_2_points, image_1_points, cv2.RANSAC,
                                           maxIters=2000)
     inliersMask = mask.ravel().tolist()
 
-    inlier_outlier_images = draw_inlier_outlier(merge_images(img1, img2), img1.shape, new_matches, inliersMask, kp1, kp2)
+    inlier_outlier_images = draw_inlier_outlier(merge_images(img1, img2), img1.shape, new_matches, inliersMask,
+                                                image_1_points, image_2_points)
     cv2.imwrite(output_path.format("res17.jpg"), inlier_outlier_images)
     print(homography)
     projected_img = cv2.warpPerspective(transforming_img, homography, (10000, 10000))
