ac8871e8f97ec4c9f76f1be0fa65aee656919334
Sahel Mesforoush <5176161-sahel.m4@users.noreply.gitlab.com>
2021-04-09 14:15:56 +0430

    final result with best h

--- HW1/q4/q4.py
+++ HW1/q4/q4.py
@@ -6,8 +6,7 @@ from matplotlib import pyplot as plt
 from scipy.linalg import lstsq
 from icecream import ic
 import math
-output_path = 'outputs/'
-
+from HW1.q3 import q31
 
 
 def img_resize(img, percent):
@@ -24,13 +23,8 @@ def load_image(image_address):
     return img
 
 
-# def compute_homography(points1, points2):
-#     h = cv2.getPerspectiveTransform(points1, points2)
-#     return h
-
-
 def compute_difference(x1, y1, x2, y2):
-    return math.sqrt((x1-x2)**2+(y1-y2)**2)
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
 
 def compute_inliers(h, points1, points2, threshold):
@@ -39,8 +33,8 @@ def compute_inliers(h, points1, points2, threshold):
     for i, p in enumerate(points1):
         xs = np.array([[points1[i][0, 0], points1[i][0, 1], 1]]).T
         predictions = np.matmul(h, xs)
-        x_pred = predictions[0, 0]/predictions[2, 0]
-        y_pred = predictions[1, 0]/predictions[2, 0]
+        x_pred = predictions[0, 0] / predictions[2, 0]
+        y_pred = predictions[1, 0] / predictions[2, 0]
         diff = compute_difference(x_pred, y_pred, points2[i][0, 0], points2[i][0, 1])
         # print(f"diff {i} : ", diff)
         if diff < threshold:
@@ -56,8 +50,8 @@ def compute_points_array(point1, point2):
     x = point2[0, 0]
     yp = point1[0, 1]
     y = point2[0, 1]
-    arr1 = [0, 0, 0, -x, -y, -1, x*yp, y*yp, yp]
-    arr2 = [x, y, 1, 0, 0, 0, -x*xp, -y*xp, -xp]
+    arr1 = [0, 0, 0, -x, -y, -1, x * yp, y * yp, yp]
+    arr2 = [x, y, 1, 0, 0, 0, -x * xp, -y * xp, -xp]
     return arr1, arr2
 
 
@@ -82,54 +76,29 @@ def compute_homography(points1, points2):
 
 def compute_lstsqr_homography(inliers):
     points1, points2 = inliers
-    # print("src points : ", points2)
+    points1 = np.array(points1, dtype=np.float32)
+    points2 = np.array(points2, dtype=np.float32)
 
-    h, m = cv2.findHomography(srcPoints=np.array(points2), dstPoints=np.array(points1))
+    # h, m = cv2.findHomography(srcPoints=np.array(points2), dstPoints=np.array(points1))
+    matrix = [[0, 0, 0, 0, 0, 0, 0, 0, 1]]
     # matrix = []
-    # result = []
-    # for j in range(len(points1)):
-    #     p1 = points1[j]
-    #     p2 = points2[j]
-    #     arr1, arr2 = compute_points_array(p1, p2)
-    #     matrix.append(arr1)
-    #     matrix.append(arr2)
-    #     result.append(0)
-    #     result.append(0)
-    # result = np.array(result)
-    # matrix = np.array(matrix)
-    # print("result : ", result.shape)
-    # print("matrix : ", matrix.shape)
-    # h, res, rnk, s = lstsq(matrix, result)
-    # U, s, V = np.linalg.svd(matrix, full_matrices=True)
-    # print(V.shape)
-    # h = V[-1, :] / V[-1, -1]
-    return h
-
-
-def draw_match(base_image, position1, position2, color, r=5, thickness=5):
-    cv2.circle(base_image, position1, r, color, thickness)
-    cv2.circle(base_image, position2, r, color, thickness)
-    cv2.line(base_image, position1, position2, color, thickness)
-
-
-def draw_inlier_outlier(draw_img, based_shape, matches, masks, img1_points, img2_points,
-                        inlier_color=(255, 0, 0), outlier_color=(0, 255, 255)):
-    for i, m in enumerate(matches):
-        end1 = (round(img1_points[i][0, 0]), round(img1_points[i][0, 1]))
-        end2 = (round(img2_points[i][0, 0] + based_shape[1]), round(img2_points[i][0, 1]))
-        if masks[i]:
-            draw_match(base_image=draw_img, position1=end1, position2=end2, color=inlier_color)
-        # else:
-        #     draw_match(base_image=draw_img, position1=end1, position2=end2, color=outlier_color)
-    return draw_img
-
-
-def merge_images(img1, img2):
-    new_shape = (max(img1.shape[0], img2.shape[0]), img1.shape[1] + img2.shape[1], img1.shape[2])
-    new_img = np.zeros(new_shape, dtype=img1.dtype)
-    new_img[:img1.shape[0], :img1.shape[1]] = img1
-    new_img[:img2.shape[0], img1.shape[1]:img1.shape[1]+img2.shape[1]] = img2
-    return new_img
+    result = [1]
+    for j in range(len(points1)):
+        p1 = points1[j]
+        p2 = points2[j]
+        arr1, arr2 = compute_points_array(p1, p2)
+        matrix.append(arr1)
+        matrix.append(arr2)
+        result.append(0)
+        result.append(0)
+    result = np.array(result, dtype=np.float32)
+    matrix = np.array(matrix, dtype=np.float32)
+    lst_h, res, rnk, s = lstsq(matrix, result)
+    lst_h = lst_h.reshape((3, 3))
+    lst_h = lst_h / lst_h[2, 2]
+    svd_h = compute_homography(points1, points2)
+    svd_h = svd_h/svd_h[2, 2]
+    return svd_h
 
 
 def ransac(points1, points2, N):
@@ -153,51 +122,52 @@ def ransac(points1, points2, N):
             best_result = (inliers1, inliers2)
             final_mask = mask
             best_h = h
-    ic(best_inlier_num)
-    ic(best_h)
     final_homography = compute_lstsqr_homography(best_result)
-
+    final_homography = final_homography / final_homography[2, 2]
     return best_h, final_mask
 
 
 if __name__ == "__main__":
     img1 = load_image("../inputs/im03.jpg")
     img2 = load_image("../inputs/im04.jpg")
-    transforming_img = img2.copy()
-    sift = cv2.SIFT_create()
-    kp1, des1 = sift.detectAndCompute(img1, None)
-    kp2, des2 = sift.detectAndCompute(img2, None)
-    bf = cv2.BFMatcher(normType=cv2.NORM_L2)
-    matches = bf.knnMatch(des1, des2, k=2)
-    new_matches = []
-    for (m, n) in matches:
-        if m.distance < 0.7 * n.distance:
-            new_matches.append(m)
-
-    image_1_points = np.zeros((len(new_matches), 1, 2), dtype=np.float32)
-    image_2_points = np.zeros((len(new_matches), 1, 2), dtype=np.float32)
-
-    for i in range(len(new_matches)):
-        image_1_points[i] = kp1[new_matches[i].queryIdx].pt
-        image_2_points[i] = kp2[new_matches[i].trainIdx].pt
-    homography, mask = ransac(image_1_points, image_2_points, 5000)
-    finlier_outlier_images = draw_inlier_outlier(merge_images(img1, img2), img1.shape, new_matches, mask,
-                                                 image_1_points, image_2_points)
-    plt.imshow(finlier_outlier_images)
-    plt.show()
-    # h_matrix = homography.reshape((3, 3))
+    new_matches, match_points, key_points = q31.compute_matching_points(img1, img2)
+    image_1_points = match_points[0]
+    image_2_points = match_points[1]
+    homography, mask = ransac(image_1_points, image_2_points, 7000)
+    # finlier_outlier_images = q31.draw_inlier_outlier(q31.merge_images(img1, img2), img1.shape, new_matches, mask,
+    #                                                  image_1_points, image_2_points)
+    # plt.imshow(finlier_outlier_images)
+    # plt.show()
     h_matrix = homography
-    h_matrix = h_matrix
-    # ic(h_matrix)
-    # h_matrix = h_matrix/homography[8]
-    # ic(h_matrix)
-    #
-    projected_im = cv2.warpPerspective(transforming_img, h_matrix, (7000, 7000))
-    plt.imshow(projected_im)
-    plt.show()
+    transforming_img = img2.copy()
+    # h_matrix = np.array([[1.7798548e+00, 6.6127861e-01, 1.8474008e+02],
+    #                      [1.1618153e+00, 1.9653734e+00, -2.3575032e+02],
+    #                      [1.1933770e-03, 9.3739229e-04, 1.0000000e+00]], dtype=np.float32)
+    projected_im = cv2.warpPerspective(transforming_img, h_matrix, (1000, 1000))
+    cv2.imwrite("res20.jpg", projected_im)
 
 """
-[[-3.72054958e+00 -9.54013943e+00  1.88899338e+03]
- [-1.31066656e+00 -9.69767633e+00  7.38441546e+02]
- [-3.11190479e-03 -1.07859328e-02  1.00000000e+00]]
+ic| best_h: array([[ 3.52557214e+00,  2.80726590e-01, -2.26172914e+03],
+                   [ 1.00424726e-01,  2.26160782e+00, -1.15416081e+03],
+                   [ 1.20562000e-04, -1.88338772e-04,  1.00000000e+00]])
+ic| h: array([ 1.2596006e-06,  4.6798590e-07,  1.3074029e-04,  8.2221499e-07,
+               1.3908918e-06, -1.6684015e-04,  8.4455110e-10,  6.6339112e-10,
+               7.0769852e-07], dtype=float32)
+ic| h_matrix: array([[ 1.7798548e+00,  6.6127861e-01,  1.8474008e+02],
+                     [ 1.1618153e+00,  1.9653734e+00, -2.3575032e+02],
+                     [ 1.1933770e-03,  9.3739229e-04,  1.0000000e+00]], dtype=float32)
+
+"""
+
+
+"""
+svd :  [[ 3.9264586e-02, -3.7883878e-02,  6.5775244e+02],
+                             [-1.6091885e-01,  3.5049862e-01,  5.3717590e+02],
+                             [-2.0622200e-04,  2.1815495e-05,  1.0000000e+00]]
+                             
+                             
+lstsqrs : [[ 1.7798548e+00,  6.6127861e-01,  1.8474008e+02],
+                             [ 1.1618153e+00,  1.9653734e+00, -2.3575032e+02],
+                             [ 1.1933770e-03,  9.3739229e-04,  1.0000000e+00]]
+
 """
