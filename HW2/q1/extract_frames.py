4fcb010c7c264643f4c0b76fc672f1030d992193
Sahel Mesforoush <5176161-sahel.m4@users.noreply.gitlab.com>
2021-04-18 21:56:33 +0430

    extracting frames

---
+++ HW2/q1/extract_frames.py
@@ -0,0 +1,22 @@
+# Importing all necessary libraries
+import cv2
+import os
+
+# Read the video from specified path
+cam = cv2.VideoCapture("../inputs/video.mp4")
+output_path = "../inputs/frame-{}.jpg"
+
+currentframe = 1
+status = True
+
+while status:
+    status, frame = cam.read()
+    name = output_path.format(currentframe)
+    print('Creating...' + name)
+    cv2.imwrite(name, frame)
+    currentframe += 1
+    if currentframe > 900:
+        break
+# Release all space and windows once done
+cam.release()
+cv2.destroyAllWindows()
