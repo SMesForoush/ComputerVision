6dbddaa2a867ffb743eb29ec17ff65f31ed9534c
Sahel Mesforoush <5176161-sahel.m4@users.noreply.gitlab.com>
2021-05-05 07:35:20 +0430

    q5 initial file

---
+++ HW2/q1/q1-5.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+import pandas as pd
+
+if __name__ == '__main__':
+    background = cv2.imread("outputs/background_frame.jpg")
+    frame = cv2.imread("../inputs/frame-1.jpg")
+    w, h, c = frame.shape
+    frame_hs = pd.read_csv("outputs/frames_h.csv")
+    frame_hs.drop(['Unnamed: 0'], axis=1, inplace=True)
+    print(frame_hs)
+    vals = frame_hs.values
+    print(vals[0])
+    print(len(vals))
+    for i in range(len(vals)):
+        flat_h = vals[i]
+        current_h = flat_h.reshape((-1, 3))
+        reverse_h = np.linalg.inv(current_h)
+        projected_img = cv2.warpPerspective(background, reverse_h, (w, h))
+        cv2.imwrite(f"outputs/background-frame-{i+1}.jpg", projected_img)
